2130e2a8c2125e5e263bb0912be5d764870bdf1c
Francisco Alves <dffrancisco@gmail.com>
2021-01-15 14:23:58 -0300

    connect up

---
+++ class/class.connect_firebird.php
@@ -0,0 +1,25 @@
+<?php
+
+class ConexaoFirebird
+{
+
+    public static function getConectar()
+    {
+        $banco = '/home/sisvop/sisvop.fdb';
+        $server = '10.0.80.38';
+        $server = 'vsescola.com.br';
+
+        try {
+            $fp = @fsockopen($server, 3050, $errno, $errstr, (float) 1.1);
+
+            if (!$fp) {
+                return false;
+            } else {
+                $pdo = new PDO("firebird:dbname=$server:$banco;charset=utf8;", "SYSDBA", base64_decode('MTUyMEFsdmVz'));
+                return $pdo;
+            }
+        } catch (PDOException $erro) {
+            die('Servidor não está respondendo ' . $erro->getMessage());
+        }
+    }
+}
