1665bbe4678646d8565d4bd002618cd685a84595
dolbsk <kallebealves37@gmail.com>
2021-03-30 16:49:47 -0300

    teste de correcao

--- servicos/per.servicos.php
+++ servicos/per.servicos.php
@@ -20,184 +20,40 @@ class Servicos
         $this->sql = new SqlServicos();
     }
 
-    // GETS
-    function getCliente($param){
-        $call = $this->sql->getCliente($param);
+    function getServico()
+    {
+        $call = $this->sql->getServico();
         echo json_encode($call);
     }
 
-    function getListaServico($param){
-        $call = $this->sql->getListaServico($param);
+    function getMascaraProjeto($param)
+    {
+        $call = $this->sql->getMascaraProjeto($param);
         echo json_encode($call);
     }
 
-    function getProdutos($param){
+    function getProdutos($param)
+    {
         $call = $this->sql->getProdutos($param);
         echo json_encode($call);
     }
 
-    function getServ(){
-        $call = $this->sql->getServ();
-        echo json_encode($call);
-    }
-
-    function getProduto($param){
-        $call = $this->sql->getProduto($param);
-        echo json_encode($call);
-    }
-
-    function getRomaneio($param){
-        $call = $this->sql->getRomaneio($param);
-        echo json_encode($call);
-    }
-    
-    function getItensRomaneio($param){
-        $call = $this->sql->getItensRomaneio($param);
-        echo json_encode($call);
-    }
-
-    function getRomaneioRela($param){
-        $call = $this->sql->getRomaneio($param);
-
-        $rs = [];
-
-            foreach ($call as $ln) {
-                $ln->offset = 0;
-                $rsIR = $this->sql->getItensRomaneio((Array) $ln);
-                $ln->ITENS = $rsIR; 
-
-                unset($ln->offset);
-                
-                $rs[] = $ln;
-            }
-
-        echo json_encode($rs);
-    }  
-
-
-    function getItens($param)
+    function insertProduto($param)
     {
-        $call = $this->sql->getItens($param);
+        $duplicity = $this->sql->duplicityProduto($param);
+        if (!empty($duplicity)) {
+            echo '{"msg":"Produto ja incluso"}';
+            return false;
+        }
+        $call = $this->sql->insertProduto($param);
         echo json_encode($call);
     }
 
-    function getItens2($param)
+
+    function deleteProduto($param)
     {
-        $call = $this->sql->getItens2($param);
+        $call = $this->sql->deleteProduto($param);
         echo json_encode($call);
-    }
-
-    function getServicos($param){
-        
-        $dados = [];
-        
-        if($param['andamento']== 'ANDAMENTO'){
-
-            $andamento = $this->sql->getServicosAnd($param);
-
-            $dados = array_merge($andamento, $dados);
-        }
-        
-        if($param['projeto']== 'PROJETO'){
-
-            $projeto = $this->sql->getServicosPro($param);
-            $dados = array_merge($projeto, $dados);
-
-        }
-        
-        if($param['finalizado']== 'FINALIZADO'){
-
-            $finalizado = $this->sql->getServicosFin($param);
-            $dados = array_merge($finalizado, $dados);
-
-        }
-        if($param['finalizado'] == '' && $param['projeto'] == '' && $param['andamento'] == ''){
-            $todos = $this->sql->getServicos($param);  
-            $dados = array_merge($todos, $dados); 
-        }
-        
-        echo json_encode($dados);
-    }
-
-    function getListaServicoX($param)
-    {
-        $call = $this->sql->getListaServicoX($param);
-        echo json_encode($call);
-    }
-
-    function getDevolucao($param){
-        $call = $this->sql->getDevolucao($param);
-        echo json_encode($call);
-    }
-
-    function getUf()
-    {
-        $call = $this->sql->getUf();
-        echo json_encode($call);
-    }
-    //NOVO E GERAR
-    function novoRomaneio($param){
-        $call = $this->sql->novoRomaneio($param);
-        echo json_encode($call);
-    }
-    
-    function gerarServico($param){
-        $call = $this->sql->gerarServico($param);
-        echo '{"ID_LISTA_SERVICO":"'.$call[0]->ID_LISTA_SERVICO.'"}';
-    }
-
-    function saveCliente($param){
-
-        $id_cliente = $this->sql->insert($param);
-        echo '{"ID_CLIENTE":"' . $id_cliente[0]->ID_CLIENTE . '"}';
-       
-    }
-
-    //INSERIR
-    function inserirItens($param){
-        $call = $this->sql->inserirItens($param);
-        echo json_encode($call);
-    }
-
-    function inserirItemRomaneio($param){
-        $call = $this->sql->inserirItemRomaneio($param);
-        $attServ = $this->sql->atualizaQtdItens($param);
-        echo json_encode($call);
-    }
-
-    function inserirDevolucao($param){
-        $call = $this->sql->inserirDevolucao($param);
-        echo json_encode($call);
-    }
-    
-
-    // UPDATE
-    function atualizaProduto($param){
-        $call = $this->sql->atualizaProduto($param);
-        echo json_encode($call);
-    }
-
-    function atualizaStatus($param){
-
-        $call = $this->sql->atualizaStatus($param);
-        echo json_encode($call);
-    }
-
-    function finalizarRomaneio($param){
-        $call = $this->sql->finalizarRomaneio($param);
-        echo json_encode($call);
-    }
-
-    //DELETE
-    function deletarItem($param){
-        $call = $this->sql->deletarItem($param);
-        echo json_encode($call);
-    }
-    
-    function deletarItemRomaneio($param){
-        $attP = $this->sql->atualizaProduto($param);
-        $attQ = $this->sql->atualizaQtdItens($param);
-        $dltI = $this->sql->deletarItemRomaneio($param);
     }
 }
 
